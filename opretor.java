650f9a0f84730d38d9517c566150e2942bbc380c
Akshay <131137051+2-akshay@users.noreply.github.com>
2024-01-26 19:16:50 +0530

    Add files via upload

---
+++ opretor.java
@@ -0,0 +1,71 @@
+public class opretor {
+    public static void main(String[] args) {
+
+        p1();
+        p2();
+        p3();
+        p4();
+        p5();
+
+    }
+       // Question : What will be the output of the following programs :
+
+
+            public static void p1(){
+                int x = 2, y = 5;
+                int exp1 = (x * y / x);
+                int exp2 = (x * (y / x));
+
+              //  System.out.print(exp1 + “ , ”);
+                System.out.print(exp2);
+            }
+
+
+
+
+            public static void p2() {
+                int x = 200, y = 50, z = 100;
+                if(x > y && y > z){
+                    System.out.println("Hello");
+                }
+                if(z > y && z < x){
+                    System.out.println("Java");
+                }
+                if((y+200) < x && (y+150) < z){
+                    System.out.println("Hello Java");
+                }
+            }
+
+
+
+            public static void p3(){
+                int x, y, z;
+                x = y = z = 2;
+                x += y;
+                y -= z;
+                z /= (x + y);
+                System.out.println(x + " " + y + " " + z);
+            }
+
+
+
+
+            public static void  p4(){
+                int x = 9, y = 12;
+                int a = 2, b = 4, c = 6;
+                int exp = 4/3 * (x + 34) + 9 * (a + b * c) + (3 + y * (2 + a)) / (a + b*y);
+
+                System.out.println(exp);
+            }
+
+
+
+
+            public static void p5(){
+                int x = 10, y = 5;
+                int exp1 = (y * (x / y + x / y));
+                int exp2 = (y * x / y + y * x / y);
+                System.out.println(exp1);
+                System.out.println(exp2);
+    }
+}
