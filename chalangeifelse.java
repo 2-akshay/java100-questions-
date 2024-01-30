d7c05775deccfc9d32479ca27645860e74676a88
Akshay <131137051+2-akshay@users.noreply.github.com>
2024-01-30 19:32:14 +0530

    Add files via upload

---
+++ chalangeifelse.java
@@ -0,0 +1,104 @@
+import java.util.Scanner;
+
+public class chalangeifelse {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        
+//   5.Create a program that determines if a number is positive, negative,
+        //or zero.
+       /* System.out.println("Enter your number");
+        int num=sc.nextInt();
+        if (num>0){
+            System.out.println(num+"given number is positive");
+        }else if (num<0){
+            System.out.println(num+"given number is negative");
+
+        }else if (num==0){
+            System.out.println(num+"given number is zero");
+        }else {
+            System.out.println("please inter valid number");
+        } */
+
+
+
+//        16.Create a program that determines if a number is odd or even.
+       /* System.out.println("Enter the number");
+        int  numb=sc.nextInt();
+        if (numb%2==0){
+            System.out.println("given number is even");
+        }else {
+            System.out.println("given number is odd");
+        } */
+
+//        17. Create a program that determines the greatest of the three
+//        numbers.
+       /* System.out.print("Enter your first number");
+        double numb1=sc.nextDouble();
+        System.out.print("Enter your second number");
+        double numb2=sc.nextDouble();
+        System.out.print("Enter your third number");
+        double numb3=sc.nextDouble();
+
+        if (numb1>numb2 && numb1>numb3)
+            System.out.println(numb1+"it is a greatest number");
+        else if (numb2>numb3) {
+            System.out.println(numb2+"it is a greatest number");
+        }else {
+            System.out.println(numb3+"it is a greatest number");
+        }*/
+//        18.Create a program that determines if a given year is a leap year
+//                (considering conditions like divisible by 4 but not 100, unless also
+//                        divisible by 400).
+
+      /*  System.out.println("welcome to leap year");
+        System.out.println("Enter the your year");
+        int year=sc.nextInt();
+
+        if (year%400==0||year%4==0&&year%100!=0){
+            System.out.println(year+ "this year is leap year");
+
+        }else {
+            System.out.println(year+ "this is not leap year");
+        }*/
+
+//        19.Create a program that calculates grades based on marks
+//        A -> above 90% B -> above 75%
+//                C -> above 60% D -> above 30%
+//                F -> below 30%
+       /* System.out.println("welcome to grades calculation");
+        System.out.println("Enter your marks");
+        float marks=sc.nextFloat();
+
+        if (marks>90){
+            System.out.println("congratulation  for A grade");
+        }else if (marks>60){
+            System.out.println("congratulation  for B grade");
+        } else if (marks>30) {
+            System.out.println("congratulation  for C grade");
+
+        } else if (marks<30) {
+            System.out.println("sorry you are fail keep try again");
+
+        }*/
+
+
+//                20.Create a program that categorize a person into different age groups
+//        Child -> below 13 Teen -> below 20
+//        Adult -> below 60 Senior-> above 60
+
+//        System.out.println("Enter your age ");
+//        int age=sc.nextInt();
+//
+//        if (age>60){
+//            System.out.println("your are senior");
+//        }else if (age<60){
+//            System.out.println("you are a Adult");
+//        }else if (age<20){
+//            System.out.println("your age  a teen ager");
+//        }else {
+//            System.out.println("you are child");
+//        }
+
+
+    }
+}
